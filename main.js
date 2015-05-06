491c925ea3c06408e391a3546bfbdc22bf3b767a
Jose Vazquez <jose-vm@users.noreply.github.com>
2015-05-05 21:29:43 -0500

    Added more basic functions
    @DanHerePE You done with model yet?

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 function newLevel(hasLevel) {
-clientMessage("Guardian Mod");
-clientMessage("by:");
-clientMessage("§7DanHerePE");
-clientMessage("§CJoePaleto");
-clientMessage("§3Xriomedabeast");
+  clientMessage("Guardian Mod");
+  clientMessage("by:");
+  clientMessage("§7DanHerePE");
+  clientMessage("§CJoePaleto");
+  clientMessage("§3Xriomedabeast");
 }
 
 function useItem(x, y, z, itemId, blockId, side) {
 }
 
 function modTick() {
+}
+
+function attackHook(attacker, victim) {
+}
+
+function deathHook(attacker, victim) {
+}
+
+function spawnGuardian(x, y, z, health, skin) { // am I doing it right?
+  var guardian = Level.spawnMob(x, y, z, 36, skin); // entityId still pending to be decided!!!
+  Entity.setHealth(guardian, health);
 }
